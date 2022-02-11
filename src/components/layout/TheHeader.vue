93e170731e9c77b8becaafa4df3cfa0fd11db6d4
IlhamArbani <ilham.na.ar@gmail.com>
2022-02-11 18:59:04 +0700

    change store sturcture

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="w-full px-28 flex justify-between bg-blue-900">
+    <header class="w-full px-28 flex justify-between bg-gray-900 items-center backdrop-opacity-10">
         <img src="../../assets/logo.png" class="h-10 w-10"/>
         <nav>
             <ul class="flex text-white">
