3c81e2a51cddfc7dd4c99c6b53cda381bb42131c
IlhamArbani <ilham.na.ar@gmail.com>
2022-02-14 16:55:30 +0700

    restructure views

---
+++ src/components/ButtonNavigation.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex p-1 bg-main-color w-1/4">
+        <button class="w-20 h-10 rounded-md text-white">All</button>
+        <button class="w-20 h-10 rounded-md text-white">Movies</button>
+        <button class="w-20 h-10 rounded-md text-white">Tv Shows</button>
+    </div>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Movies from '../views/Movies/index.vue'
+import Home from '../views/pages/Home.vue'
+import TvShows from '../views/TvShows/index.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: Movies }
+        {
+            path: '/', component: Home, children: [
+                { path: '', component: Movies },
+                { path: 'tvshows', component: TvShows }
+            ]
+        }
     ]
 });
 
